d8eea42fe7e7fe8fd23e7b078cce16e4401724fc
Pasha-kto003 <lukovnikov.pasha@gmail.com>
2020-11-02 17:47:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+       class program
+        {
+            static void Main(string[] args)
+            {
+              Cat  c = new Cat("Vladik", new DateTime(2010, 05, 25));
+                c.MakeNoise();
+                Console.WriteLine($"Кошке по имени {c.Name} уже {c.GetAge()} лет");
+                c.HungryStatus = 150;
+                Console.ReadLine();
+            }
+        }
+    }
+    
+
